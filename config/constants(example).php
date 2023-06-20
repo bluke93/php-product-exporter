d84574c7ff87fbfcc487229d34d4410462213f08
Luke <baroneluca93@gmail.com>
2023-06-20 03:02:15 +0200

    example config

---
+++ config/constants(example).php
@@ -0,0 +1,16 @@
+<?php 
+
+//
+define("BASE_URL", "easyapi5.eswportal.it");
+define("API_PATH", "api");
+define("API_VERSION", "v1");
+define("DB_CONNECTION", "252_OVM_VICENZA");
+define("STORE_ID", "EC");
+define("API_ENDPOINT", "/listini/articoli/azienda/{codice_negozio}/modificati/{startDateString}?webgallery=true");
+define("HEADER_APIKEY_KEY", "X-ApiKey");
+define("HEADER_APIKEY_VALUE", "SdEFLRMq+U+8BI2yn5O11EJ4YWJHWlY4N1VTTWYwM2NRZUZOSmc=");
+define("MAX_RETRIES", 5);
+define("IMAGES_PATH", 'images');
+define("EXPORTED_PATH", 'exports');
+
+?>
